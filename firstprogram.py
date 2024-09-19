316a23b1daacb19efa8f497db585a094fde44830
J Park <mrparkonline@gmail.com>
2024-09-19 09:33:52 -0400

    HOLY F I MADE A CALCULATOR!!

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,8 @@
-# this is a comment
-print("Hello, World!!")
+# Basic Calculator
+
+print("Hey Fam, welcome to my bussin Calculator!")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+answer = num1 + num2
+print(f"Your answer is: {answer}!!!")
